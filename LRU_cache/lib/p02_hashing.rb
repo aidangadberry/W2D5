c56baa4ac55848b44e676ac31b7b23a51e510883
gusuly0rum <donguk.kim1112@gmail.com>
2018-06-29 15:17:46 -0700

    Finish hasing for array, string, and hash

--- LRU_cache/lib/p02_hashing.rb
+++ LRU_cache/lib/p02_hashing.rb
@@ -4,11 +4,25 @@ end
 
 class Array
   def hash
+    result = 0
+    
+    self.each_with_index do |el, idx|
+      result += (el.object_id * idx.object_id) ** 2
+    end
+    
+    result.hash
   end
 end
 
 class String
   def hash
+    result = 0
+    
+    self.each_byte.with_index do |c, idx|
+      result += (c.object_id * idx.object_id) ** 2
+    end
+    
+    result.hash
   end
 end
 
@@ -16,6 +30,6 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    self.keys.sort.map { |el| el.object_id + self[el].hash.object_id }.hash
   end
 end
