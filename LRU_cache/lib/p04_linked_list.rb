7e51b216a277ede4be824ac8e4a4cbac5ccca30d
gusuly0rum <donguk.kim1112@gmail.com>
2018-06-29 16:37:18 -0700

    Finish LinkedList

--- LRU_cache/lib/p04_linked_list.rb
+++ LRU_cache/lib/p04_linked_list.rb
@@ -1,3 +1,5 @@
+
+
 class Node
   attr_accessor :key, :val, :next, :prev
 
@@ -13,13 +15,20 @@ class Node
   end
 
   def remove
-    # optional but useful, connects previous link to next link
-    # and removes self from list.
+    @next.prev = @prev
+    @prev.next = @next
+    @prev, @next = nil
   end
 end
 
 class LinkedList
+  include Enumerable
+  
   def initialize
+    @head = Node.new
+    @tail = Node.new
+    @head.next = @tail
+    @tail.prev = @head
   end
 
   def [](i)
@@ -28,34 +37,61 @@ class LinkedList
   end
 
   def first
+    @head.next
   end
 
   def last
+    @tail.prev
   end
 
   def empty?
+    @head.next == @tail
   end
 
   def get(key)
+    self.each do |node|
+      return node.val if node.key == key
+    end
   end
 
   def include?(key)
+    self.each do |node|
+      return true if node.key == key
+    end
+    false
   end
 
   def append(key, val)
+    node = Node.new(key, val)
+    node.next = @tail
+    node.prev = @tail.prev
+    @tail.prev.next = node
+    @tail.prev = node
   end
 
   def update(key, val)
+    self.each do |node|
+      node.val = val if node.key == key
+    end
   end
 
   def remove(key)
+    self.each do |node|
+      node.remove if node.key == key
+    end
   end
 
   def each
+    curr_link = first
+    
+    until curr_link == @tail
+      break if curr_link.nil?
+      yield curr_link
+      curr_link = curr_link.next
+    end
   end
 
-  # uncomment when you have `each` working and `Enumerable` included
-  # def to_s
-  #   inject([]) { |acc, link| acc << "[#{link.key}, #{link.val}]" }.join(", ")
-  # end
+  def to_s
+    inject([]) { |acc, link| acc << "[#{link.key}, #{link.val}]" }.join(", ")
+  end
 end
