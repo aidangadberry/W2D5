1f4e33aad0e7a51bb39b2613d5d4eebab3283841
gusuly0rum <donguk.kim1112@gmail.com>
2018-06-29 15:27:21 -0700

    Hash keys before indexing

--- LRU_cache/lib/p03_hash_set.rb
+++ LRU_cache/lib/p03_hash_set.rb
@@ -6,25 +6,45 @@ class HashSet
     @count = 0
   end
 
-  def insert(key)
+  def insert(num)
+    resize! if @count == num_buckets
+
+    unless include?(num)
+      self[num] << num
+      @count += 1
+    end
   end
 
-  def include?(key)
+  def remove(num)
+    if include?(num)
+      self[num].delete(num)
+      @count -= 1
+    end
   end
 
-  def remove(key)
+  def include?(num)
+    self[num].include?(num)
   end
 
   private
 
   def [](num)
-    # optional but useful; return the bucket corresponding to `num`
+    @store[num.hash % num_buckets]
   end
 
   def num_buckets
     @store.length
   end
 
+
   def resize!
+    resized = Array.new(num_buckets * 2) { Array.new }
+    @store.each do |bucket|
+      bucket.each do |el|
+        resized[el % (num_buckets * 2)] << el
+      end
+    end
+
+    @store = resized
   end
 end
